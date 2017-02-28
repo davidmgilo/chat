a33e2ce58fa1b7a920e62f663028d589db6d1162
David Martinez <davidmgilo@gmail.com>
2017-02-28 20:10:57 +0100

    fillable

--- app/Message.php
+++ app/Message.php
@@ -6,7 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'message',
+    ];
 
     /**
      * A message belong to a user
