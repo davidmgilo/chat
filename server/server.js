6d92d6621cb17410298d724c467a28973b67aff6
David Martinez <davidmgilo@gmail.com>
2017-03-21 20:28:44 +0100

    server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var Redis = require('ioredis');
 
+var redis = new Redis();
+
+http.listen(3000, function(){
+   console.log('Listening at port 3000')
+});
