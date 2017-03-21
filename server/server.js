143ebc741eec46d73e1aabecbe23ca30a172c7b8
David Martinez <davidmgilo@gmail.com>
2017-03-21 20:43:51 +0100

    server

--- server/server.js
+++ server/server.js
@@ -5,6 +5,21 @@ var Redis = require('ioredis');
 
 var redis = new Redis();
 
+redis.psubscribe('*', function(err, count) {
+    console.log('Subscribing to channels: number of channels ' + count);
+    if(err){
+       console.log('Error subscribing to channel ' + err)
+    }
+});
+
+redis.on('pmessage', function(subscribed,channel, message) {
+    console.log('Message Recieved at channel(' + channel + '): ' + message);
+    message = JSON.parse(message);
+    io.emit(channel + ':' + message.event, message.data);
+});
+
+io.emit('chat:missatge');
+
 http.listen(3000, function(){
    console.log('Listening at port 3000')
 });
